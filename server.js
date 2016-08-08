992d8a8f83fdec37f3efc9a643d97ab823afb05e
jphire <janne.laukkanen83@gmail.com>
2016-08-08 23:39:15 +0300

    add img folder

--- server.js
+++ server.js
@@ -1,30 +1,61 @@
 'use strict'
 
-let fs = require('fs')
-let http = require('http')
-let url = require('url')
+let fs = require('fs');
+let http = require('http');
+let url = require('url');
+
+var imageToSlices = require('image-to-slices');
+imageToSlices.configure({
+    clipperOptions: {
+        canvas: require('canvas')
+    }
+});
 
 let dummyArr = [67108864];
 dummyArr.fill(2);
 
 let server = http.createServer(function(request, response){
-    var url_parts = url.parse(request.url).path.substring(1).split("/");
+    var parts = url.parse(request.url, true);
+    var url_parts = parts.path.substring(1).split("/");
     var action = request.pathname;
-
-	var type = url_parts[0];
-	var size = parseInt(url_parts[1]);
- 	if (!isNaN(size)) {
+    var query = parts.query;
+    var feed = url_parts[2]
+    var size = parseInt(query.size);
+    var sliced = query.nodes ? true : false;
+    var img;
+    var nodes = parseInt(query.nodes);
+    var nodeIndex = parseInt(query.index);
+    let blockSize = size/nodes;
+    var lineXArray = Array.apply(null, Array(nodes)).map(function (x, i, thisArr) { return (i+1)*blockSize; })
+    console.log(lineXArray)
+    if (!isNaN(size)) {
         if (request.method === 'GET') {
-            if (type === 'array') {
-        		let arr = dummyArr.slice(0, size);
-                response.writeHead(200, {'content-type': 'application/json'});
-                response.end(JSON.stringify(arr));
-        	} else if (type === 'image' && !isNaN(size)) {
+            if (!isNaN(size)) {
         		try {
-                    let img = fs.readFileSync('./' + size + '.jpg');
-                	response.writeHead(200, {'content-type': 'image/jpg'});
-                	response.end(img, 'binary');
+                    let source = './img/' + size + '.jpg';
+
+                    if (sliced) {
+                        // let lineXArray = [size].fill(function ());
+                        // Vertical division not yet implemented
+                        let lineYArray = [];
+
+                        imageToSlices(source, lineXArray, lineYArray, {
+                            saveToDataUrl: true
+                        }, function(dataUrlList) {
+                            //console.log(dataUrlList);
+                            let url = dataUrlList[nodeIndex].dataURI.replace(/^data:image\/(png|gif|jpeg);base64,/,'');
+                            let img = new Buffer(url, 'base64');
+                            
+                            response.writeHead(200, {'content-type': 'image/jpg'});
+                            response.end(img, 'binary');
+                        });
+                    } else {
+                        img = fs.readFileSync(source);
+                    	response.writeHead(200, {'content-type': 'image/jpg'});
+                    	response.end(img, 'binary');
+                    }
                 } catch (err) {
+                    console.log(err)
                     response.writeHead(400, {'content-type': 'application/json'});
                     response.end(JSON.stringify({'Error':'Invalid size, could not open file. Supported sizes: 256,512,1024,2048,4096'}));
                 }
